8cccfaae4c2ed104483e8ab8caefaf4a6a6f7f63
alik <alik.ozmanov@hotmail.com>
2023-11-30 17:43:33 +0100

    OK

--- config/cors.php
+++ config/cors.php
@@ -1,13 +1,13 @@
 <?php
 
-// Cette configuration CORS autorisant toutes les origines, toutes les méthodes
+// CORS est une mesure de sécurité pour contrôler les requêtes web entre domaines.
 
 return [
-    'paths' => ['*'], // '*' signifie que toutes les ROUTES sont AUTORISEES pour les requêtes CORS (cross-origin).
-    'allowed_methods' => ['*'], // '*' signifie que toutes les METHODES HTTP sont AUTORISEES (GET, POST, PUT, DELETE)
-    'allowed_origins' => ['*'], // '*' signifie que toutes les ORIGINES (ou DOMAINES) sont AUTORISEES à accéder aux ressources cross-origin.
-    'allowed_headers' => ['*'], // '*' signifie que tous les EN-TETES HTTP sont AUTORISEES pour les requêtes cross-origin.
-    'exposed_headers' => [], // Aucun EN-TETE HTTP spécifique n'est exposé (rendu visible) pour les réponses cross-origin.
-    'max_age' => 0, // Le temps maximum de mise en cache pour les informations de pré-vérification CORS est défini à 0,
-    'supports_credentials' => false, // Les informations d'identification ne sont pas prises en charge pour les requêtes cross-origin
+    'paths' => ['*'], // Toutes les ROUTES autorisées pour les requêtes CORS.
+    'allowed_methods' => ['*'], // Toutes les METHODES HTTP autorisées (GET, POST, PUT, DELETE).
+    'allowed_origins' => ['*'], // Toutes les ORIGINES autorisées à accéder aux ressources cross-origin.
+    'allowed_headers' => ['*'], // Tous les EN-TÊTES HTTP autorisés pour les requêtes cross-origin.
+    'exposed_headers' => [], // Aucun EN-TÊTE HTTP exposé pour les réponses cross-origin.
+    'max_age' => 0, // Mise en cache CORS désactivée.
+    'supports_credentials' => false, // Informations d'identification non prises en charge pour les requêtes cross-origin.
 ];
